4d09197f07a876b39e09b8a5161d4e8b3f781a2e
brandonchaffee <brandon@patterncapture.com>
2018-06-05 14:19:50 -0700

    fixed demtokenv1 to pass test

--- contracts/test/DemocraticTokenV1.sol
+++ contracts/test/DemocraticTokenV1.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.23;
 
 import "../statutory/DemocraticUpgrading.sol";
 
-contract DemTokenV1 is  UpgradingProposal {
+contract DemTokenV1 is  DemocraticUpgrading {
     constructor() public {
 
     }
