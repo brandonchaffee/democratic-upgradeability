83f3f679f88bdf92386f9b07b0830b14bf656414
brandonchaffee <brandon@patterncapture.com>
2018-06-07 12:10:55 -0700

    test changes prior to statutory voting integration into repo

--- contracts/test/DemocraticTokenV1.sol
+++ contracts/test/DemocraticTokenV1.sol
@@ -3,7 +3,19 @@ pragma solidity ^0.4.23;
 import "../DemocraticUpgrading.sol";
 
 contract DemTokenV1 is  DemocraticUpgrading {
+    bool internal _initialized;
+
     constructor(uint256 _window, uint256 _totalSupply) public
     DemocraticUpgrading(_window, _totalSupply) {
     }
+
+    function initialize(uint256 _supply, uint256 _window) public {
+        //These initialization arguments should be hardcoded
+        require(!_initialized);
+        totalSupply_ = _supply;
+        balances[msg.sender] = _supply;
+        windowSize = _window;
+        _initialized = true;
+    }
+
 }
--- test/UpgradingToken.js
+++ test/UpgradingToken.js
@@ -1,4 +1,4 @@
-// import assertRevert from './helpers/assertRevert'
+import assertRevert from './helpers/assertRevert'
 
 const DemTokenV1 = artifacts.require('DemTokenV1')
 const Proxy = artifacts.require('InitializedProxy')
@@ -9,21 +9,47 @@ contract('Democratic Upgradeablity', function (accounts) {
   beforeEach(async function () {
     this.token = await DemTokenV1.new(windowTime, totalSupply)
     this.proxy = await Proxy.new(this.token.address)
+    this.proxiedToken = DemTokenV1.at(this.proxy.address)
   })
-  describe('Initalization', function () {
-    it('has correct implementation address', async function () {
+  describe('Pre-Initialization', function () {
+    it('does not have balance or window', async function () {
+      const balanceValue = await this.proxiedToken.balanceOf(accounts[0])
+      const windowValue = await this.proxiedToken.windowSize()
+      assert.equal(balanceValue.toNumber(), 0)
+      assert.equal(windowValue.toNumber(), 0)
+    })
+    it('proxy has correct implementation address', async function () {
       const implementation = await this.proxy.implementation()
       assert.equal(implementation, this.token.address)
     })
+  })
+  describe('Post-Initialization', function () {
+    beforeEach(async function () {
+      await this.proxiedToken.initialize(totalSupply, windowTime)
+    })
     it('sender has balance', async function () {
-      // const proxiedToken = DemTokenV1.at(this.proxy.address)
-      // const balanceValue = await proxiedToken.balanceOf(accounts[0])
-      // Needs to be initailzied with token constructor stuff
-      // assert.equal(balanceValue.toNumber(), totalSupply)
+      const balanceValue = await this.proxiedToken.balanceOf(accounts[0])
+      assert.equal(balanceValue.toNumber(), totalSupply)
+    })
+    it('window has value', async function () {
+      const windowValue = await this.proxiedToken.windowSize()
+      assert.equal(windowValue.toNumber(), windowTime)
+    })
+    it('reverts on reinitialization attempt', async function () {
+      await assertRevert(this.proxiedToken.initialize(500, 50))
     })
   })
+  describe('Proposing', function () {
+    // it('should b proposal')
+  })
   describe('Voting', function () {
-
+    beforeEach(async function () {
+      this.proxiedToken.transfer(10000, accounts[1])
+      this.proxiedToken.transfer(10000, accounts[2])
+      this.proxiedToken.transfer(10000, accounts[3])
+      this.proxiedToken.transfer(10000, accounts[4])
+    })
+    // it('')
   })
   describe('Upgrading', function () {
 
